4de2901dce8e9e020a902a8d40c160964a0ac783
mt02v <141305346+mt02v@users.noreply.github.com>
2023-10-08 16:02:17 +0000

    [Add] 9章【redirect_toとrenderの処理の違いについて】

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,9 @@
 class List < ApplicationRecord
+#Listモデルに画像を扱うためのimageカラムが追記されたかのように扱うことができる
+  has_one_attached :image
+  
+  #バリデーションの設定
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-#データを追加(保存)するためのルーティングは生成されていないため、データを追加するためのルーティングを追記
-  post 'lists' => 'lists#create'  #ここに追記
-  get 'lists' => 'lists#index'
-  #.../lists/1や .../lists/3に該当
-  #as:オプションを追加することで'lists#show'の設定を、listとして利用
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit' , as: 'edit_list'
-  patch 'lists/:id' => 'lists#update' , as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 #URL「top」にアクセスしたとき、homesコントローラのtopアクションが呼び出されるように設定
 get 'top' => 'homes#top'
+resources :lists
 end
